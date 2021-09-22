3feb7cb6bb941a58e7340144961311ff9a36272d
kevin <iqkevin@aliyun.com>
2021-09-22 17:11:12 +0800

    滚动条BUG修复

--- src/views/Enphone/phoneindex/index.vue
+++ src/views/Enphone/phoneindex/index.vue
@@ -2416,7 +2416,7 @@ export default {
           if(!$this.scrollPosition.isMouseDown&&event.target.className=="scroll-panel"){// 非鼠标按下状态，为竖向滚动条触发的滚动事件
             var scrTop = event.target.scrollTop;
             var tableFixedRightDom = document.querySelector(".SiteTable .el-table__fixed-right");
-            if(scrTop!=0){
+            if($this.scrollTable.fixedTopHeight!=0){
               if(scrTop>=$this.scrollTable.fixedTopHeight){// 头部需要固定
                 $this.scrollPosition.isFixed = true;
                 var tableHeaderStyle = "width:"+$this.scrollPosition.width+"px;"
