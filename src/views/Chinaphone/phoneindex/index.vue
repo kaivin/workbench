b82270e61b112bd8ac08744326143417f6d79f1c
zhangsan321 <1028339639@qq.com>
2024-11-09 09:54:05 +0800

    feat: 质量判定分数搜索加几个值

--- src/views/Chinaphone/phoneindex/index.vue
+++ src/views/Chinaphone/phoneindex/index.vue
@@ -615,6 +615,11 @@ export default {
         {label:"1.3",value:"1.3"},
         {label:"1.4",value:"1.4"},
         {label:"1.5",value:"1.5"},
+        {label:"1.6",value:"1.6"},
+        {label:"1.7",value:"1.7"},
+        {label:"1.8",value:"1.8"},
+        {label:"1.9",value:"1.9"},
+        {label:"2.0",value:"2.0"},
       ],
       infoData:{
         totalCount:0,
